2d7b24a4d3bb543d22dc04c9b72059ffe5018a8a
Koketso-dax <lanchaster.k@gmail.com>
2023-08-10 12:35:47 -0700

    Printf instead

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+/**
+ * main - entry point returns int void args
+ *
+ * return : will be 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
