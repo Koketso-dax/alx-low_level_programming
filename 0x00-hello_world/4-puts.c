735ce9a6c316f01282da0a90515a65eb1528af70
Koketso-dax <lanchaster.k@gmail.com>
2023-08-10 12:22:35 -0700

    First c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Main funtion returns int void args
+ *
+ * Return: Always 0 = Success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
