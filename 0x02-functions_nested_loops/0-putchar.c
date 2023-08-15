d561434efd4e712fd213dce90526271164dd39f2
Koketso-dax <lanchaster.k@gmail.com>
2023-08-15 10:25:32 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main - will print out _putchar followed by a new line.
+ * 
+ * Return: Should be 0 to indicate success.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return(0);	
+}
