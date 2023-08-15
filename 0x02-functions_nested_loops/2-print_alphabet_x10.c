454080774a7c144192d142b39e6e9ea3bea920b5
Koketso-dax <lanchaster.k@gmail.com>
2023-08-15 13:55:47 -0700

    Alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - entry point function prototyped in main.h
+ * description: will print out the alphabet (a-z) ten times followed by a newline.
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		print_alphabet();
+	}
+}
