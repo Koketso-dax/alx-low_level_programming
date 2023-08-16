852f640eafde3dc027e9bc889c0134d210e280c9
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 04:10:07 -0700

    Added times table

---
+++ 0x02-functions_nested_loops/9-times-table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - prints out times table of 9 (multiples)
+ */
+
+void times_table(void)
+{
+	int x = 0;
+	int y = 0;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			printf("%d", x * y);
+		}
+		putchar('\n');
+	}
+}
