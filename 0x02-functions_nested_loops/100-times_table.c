4e949c80bd9d2898163d47d71410b0e6f3885315
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 13:40:09 -0700

    Fixed formatting

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_times_table - implements overloaded function in main.h
@@ -8,28 +7,27 @@
 
 void print_times_table(int n)
 {
-    int a, b;
-    if (n < 0 || n > 15)
-        return;
+	int a, b;
 
-    for(a = 0; a <= n; a++)
-    {
-        for(b = 0; b <= n; b++)
-        {
-            int rep = a * b;
+	if (n < 0 || n > 15)
+		return;
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			int c = a * b;
 
-            if (b == 0)
-                _putchar('0' + rep);
-            else if (rep < 10)
-                _putchar(' '), _putchar(' '), _putchar('0' + rep);
-            else if (rep < 100)
-                _putchar(' '), _putchar('0' + rep / 10), _putchar('0' + rep % 10);
-            else
-                _putchar('0' + rep / 100), _putchar('0' + (rep / 10) % 10), _putchar('0' + rep % 10);
-
-            if (b < n)
-                _putchar(','), _putchar(' ');
-        }
-        _putchar('\n');
-    }
+			if (b == 0)
+				putchar('0' + c);
+			else if (c < 10)
+				putchar(' '), putchar(''), putchar('0' + c);
+			else if (c < 100)
+				putchar(' '), putchar('0' + c / 10), putchar('0' + c % 10);
+			else
+				putchar('0' + c / 100), putchar('0' + (c / 10) % 10), putchar('0' + c % 10);
+			if (b < n)
+				putchar(','), putchar(' ');
+		}
+		putchar('\n');
+	}
 }
