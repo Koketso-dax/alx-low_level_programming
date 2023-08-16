ca1fdbdabed54e219c903a8996fc282b8efc8f0f
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 15:30:33 -0700

    advanced fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - access point
+ * description: prints out first 98 fibonacci numbers
+ * Return: must be 0.
+ */
+int main(void)
+{
+	unsigned long a = 1;
+	unsigned long b = 2;
+	unsigned long c = 0;
+	int x = 0;
+
+	printf("%lu, %lu, ", a, b);
+	while (x < 96)
+	{
+		c = a + b;
+		printf("%lu", c);
+		if (x < 95)
+		{
+			printf(", ");
+		}
+		a = b;
+		b = c;
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
