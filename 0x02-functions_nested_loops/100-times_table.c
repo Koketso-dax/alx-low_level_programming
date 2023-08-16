5e7cfe2b3ecf4eeca9d460622ab9023f6701e8b0
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 11:42:57 -0700

    Fixed typo

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,13 +9,12 @@
 void print_times_table(int n)
 {
     int a, b;
-
     if (n < 0 || n > 15)
         return;
 
-    for (a = 0; a <= n; a++)
+    for(a = 0; a <= n; a++)
     {
-        for (b = 0; b <= n; b++)
+        for(b = 0; b <= n; b++)
         {
             int rep = a * b;
 
