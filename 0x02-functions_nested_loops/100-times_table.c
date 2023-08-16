16872b8562f5a68b5236bc21a226382f0fc455b7
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 05:56:55 -0700

    Advanced time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - implements overloaded function in main.h
+ * @n: input number
+ * description: prints out times table of input
+ */
+
+void print_times_table(int n)
+{
+	int x;
+
+	if (n < 0 || n > 15)
+	{
+		break;
+	}
+	else
+	{
+		for (x = 0; x <= n; x++)
+		{
+			if (x == n)
+			{
+				printf("%d", x * n);
+			}
+			else
+			{
+				printf("%d, ", x * n);
+			}
+		}
+		_putchar('\n');
+	}
+}
