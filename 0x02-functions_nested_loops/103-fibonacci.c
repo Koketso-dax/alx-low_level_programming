04446799b9a0ec6f9350ac4573d63cbd1e3ee996
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 15:21:16 -0700

    advanced fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - prints sum of even fibonacci numbers
+*Return: return 0
+*/
+int main(void)
+{
+	unsigned long a = 0;
+	unsigned long b = 1;
+	unsigned long c = 0;
+	unsigned long sum = 0;
+
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if ((n1 % 2) == 0)
+			sum += c;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
