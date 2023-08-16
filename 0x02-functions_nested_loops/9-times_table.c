50a6689c7b71c3662ada5b30a08c8edc31cf95af
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 04:28:33 -0700

    Used putchar for formatting

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * times_table - prints out times table of 9 (multiples)
  */
@@ -8,14 +8,27 @@ void times_table(void)
 {
 	int x = 0;
 	int y = 0;
+	int tmp = 0;
 
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			printf("%d, ", x * y);
+			tmp = x * y;
+			_putchar(44);
+			_putchar(32);
+			if (tmp <= 9)
+			{
+				_putchar(32);
+				_putchar(tmp + 48);
 
+			}
+			else
+			{
+				_putchar((tmp / 10) + 48);
+				_putchar((tmp % 10) + 48);
+			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
