3cdb5c9b6bd007421b8b2d7b023385ffcc3c5232
Koketso-dax <lanchaster.k@gmail.com>
2023-08-16 05:04:42 -0700

    Accounted for decrement and conditional formatting

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 {
 	while (n <= 98)
 	{
-		putchar(n);
+		printf("%d", n);
 		if (n != 98)
 		{
 			printf("%s", ", ");
@@ -18,9 +18,9 @@ void print_to_98(int n)
 		n++;
 	}
 
-	while (n > 98)
+	while (n >= 98)
 	{
-		putchar(n);
+		printf("%d", n);
 		if (n != 98)
 		{
 			printf("%s", ", ");
