d58e886fa97efa047b1a905be09d5d93d6de6084
Koketso-dax <lanchaster.k@gmail.com>
2023-08-15 18:24:48 -0700

    Added time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes of the day from 00:00 - 23:59
+ */
+
+void jack_bauer(void)
+{
+	int hr1 = 0;
+	int hr2 = 0;
+	int sec1 = 0;
+	int sec2 = 0;
+
+	for (hr; hr < 3;hr++)
+	{
+		for (hr2; hr2 <= 9; hr2++)
+		{
+			if (hr1 == 2 && hr2 == 4)
+				return;
+			for (sec1; sec1 < 6; sec1++)
+			{
+				for (sec2; sec2 <= 9; sec2)
+				{
+					printf("%d%d:%d%d\n", hr1, hr2, sec1, sec2);
+				}
+			}
+		}
+	}
+}
