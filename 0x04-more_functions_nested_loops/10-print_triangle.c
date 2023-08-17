ded1bf67154a3f3012d4d249f85c72861381c61b
Koketso-dax <lanchaster.k@gmail.com>
2023-08-17 14:34:32 -0700

    add 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_triangle - will print triangle of specified size
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = x; y < size; y++)
+		{
+			_putchar(' ');
+		}
+		for (z = 1; z <= x; z++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
