573b4d90b82a280ad0af76bb6d134324eeb68210
Koketso-dax <lanchaster.k@gmail.com>
2023-08-17 09:28:01 -0700

    Add print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+/**
+ * print_numbers - will print numbers from 0 - 9 followed by a new line.
+ */
+void print_numbers(void)
+{
+	int x;
+	for (x = 0;x < 9;x++)
+	{
+		_putchar(x);
+	}
+	_putchar('/n')
+}
