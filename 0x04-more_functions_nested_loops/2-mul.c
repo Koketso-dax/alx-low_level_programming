7dc40eadb4d650ecdbd3739188e0a30a6e383cf3
Koketso-dax <lanchaster.k@gmail.com>
2023-08-17 09:15:29 -0700

    Added 2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - will return product of 2 int.
+ * @a: 1st int input arg
+ * @b: 2nd int input arg
+ * Return: product of a and b (int)
+ */
+int mul(int a, int b)
+{
+	return(a * b);
+}
