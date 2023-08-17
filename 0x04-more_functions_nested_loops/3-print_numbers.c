b37796ff0fd7bf98a52ed97c575ece6cb87dcf8b
Koketso-dax <lanchaster.k@gmail.com>
2023-08-17 09:40:05 -0700

    Fixed print_numbers putchar

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * print_numbers - will print numbers from 0 - 9 followed by a new line.
  */
+
 void print_numbers(void)
 {
 	int x;
+
 	for (x = 48;x < 58;x++)
 	{
 		_putchar(x);
