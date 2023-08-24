58acde37150d09ea07a2ce5e9c768a0140e430de
Koketso-dax <lanchaster.k@gmail.com>
2023-08-24 04:06:42 -0700

    Added 7-leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+/**
+ * leet - will encode str into numbers
+ * @s: input str
+ * Return: Encoded str
+ */
+char *leet(char *s)
+{
+	char new_val[128];
+	int x;
+
+	new_val['a'] = new_val['A'] = '4';
+	new_val['e'] = new_val['E'] = '3';
+	new_val['o'] = new_val['O'] = '0';
+	new_val['t'] = new_val['T'] = '7';
+	new_val['l'] = new_val['L'] = '1';
+
+    for (x = 0; s[x] != '\0'; x++) {
+        if (new_val[(int)s[x]] != 0) {
+            s[x] = new_val[(int)s[x]];
+        }
+    }
+    return (s);
+}
