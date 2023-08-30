1bda97d8a45264cc4772a9ae43af86c56e34586c
Koketso-dax <lanchaster.k@gmail.com>
2023-08-30 04:00:44 -0700

    Added puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - will recursively print out a str
+ * @s: input str
+ *
+ */
+
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion is kinda fun 🙂
