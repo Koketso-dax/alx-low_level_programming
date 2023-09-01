93c2af9db417be11e8089be1ed116f1b65be01e8
Koketso-dax <lanchaster.k@gmail.com>
2023-09-01 04:48:03 -0700

    added 1-args

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - writes its name to the command line
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n",argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - will print number of args
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
