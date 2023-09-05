a50fb015efe634ca1aa4ec492f8ad7a9e84b49bb
Koketso-dax <lanchaster.k@gmail.com>
2023-09-05 07:30:29 -0700

    Fixed strdup output

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,15 @@
 char *_strdup(char *str)
 {
 	int len = strlen(str);
-	char *s = (char *)malloc(sizeof(char) * (len + 1));
+	char *s;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+	s = malloc(sizeof(char) * (len + 1));
 	strcpy(s, str);
+
 	if (s == NULL)
 	{
 		return (NULL);
