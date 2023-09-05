a279f744dd80f06291db0a63ef73a6ac08a72cc0
Koketso-dax <lanchaster.k@gmail.com>
2023-09-05 08:18:05 -0700

    Fixed concat, added additional condition

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,6 +28,10 @@ char *str_concat(char *s1, char *s2)
 
 	len = strlen(s1) + strlen(s2) + 1;
 	s3 = malloc(sizeof(*s1) * len);
+	if (s3 == NULL)
+	{
+		return (NULL);
+	}
 	strcpy(s3, s1);
 	strcat(s3, s2);
 	return (s3);
