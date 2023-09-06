6ab5b41f3a3e867f053aca19c64afa1701c1c809
Koketso-dax <lanchaster.k@gmail.com>
2023-09-06 08:48:05 -0700

    Fixed change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,24 +8,25 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents = atoi(argv[1]);
+	int coin_values[] = {25, 10, 5, 2, 1};
+	int cents, i;
 	int coins = 0;
-	int x;
-	int dim[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("%s\n", "Error");
 		return (1);
 	}
-
-	for (x = 0; x < 5; x++)
+	cents = atoi(argv[1]);
+	if (cents <= 0)
 	{
-		while (cents >= dim[x])
-		{
-			cents -= dim[x];
-			coins++;
-		}
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 0; cents > 0; i++)
+	{
+		coins += cents / coin_values[i];
+		cents %= coin_values[i];
 	}
 	printf("%d\n", coins);
 	return (0);
