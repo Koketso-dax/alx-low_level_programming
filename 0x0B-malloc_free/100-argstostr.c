6d17629c6937dbb48368db763ef535d5cf455376
Koketso-dax <lanchaster.k@gmail.com>
2023-09-06 13:34:57 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - returns *ptr -> str or Null if fail
+ * @ac: num of args
+ * @av: [] of args
+ * Return: ptr* to str output or NULL on fail.
+ */
+char *argstostr(int ac, char *av[])
+{
+	int len = 0;
+	int x;
+	char *str;
+
+	if (ac <= 1 || av == NULL)
+	{
+		return (0);
+	}
+
+	for (x = 1; x < ac; x++)
+	{
+		len += strlen(av[x]) + 1;
+	}
+
+	str = (char *)malloc(len);
+
+	if (str == NULL)
+	{
+		free(str);
+		return (0);
+	}
+
+	str[0] = '\0';
+
+	for (x = 1; x < ac; x++)
+	{
+		strcat(str, av[x]);
+		if (x < (ac - 1))
+		{
+			strcat(str, "\n");
+		}
+	}
+
+	strcat(str, "\n");
+
+	return (str);
+}
