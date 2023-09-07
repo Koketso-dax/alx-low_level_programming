ab8fd6d49a65619793e09fa6e9c6af040902a055
Koketso-dax <lanchaster.k@gmail.com>
2023-09-07 02:12:15 -0700

    Added more alloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - will allocate mem or return 98 status on fail
+ * @b: memory amount to be allocated
+ * Return: malloc or 98 on fail
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	mem = malloc(b);
+	
+	if (!mem)
+	{
+		exit(98);
+	}
+	return (mem);
+}
