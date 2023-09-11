49a3e4f9bd2c81e0a8a36b30fb49505eec58c329
Koketso-dax <lanchaster.k@gmail.com>
2023-09-11 09:07:26 -0700

    Fixed directives

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifdef _function_like_macro_h_
+#ifndef _function_like_macro_h_
 #define _function_like_macro_h_
 
 #define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
 
-#endif
+#endif /* _function_like_macro_h_ */
