e758fee7b94a6262774a4fff0df3ec39b1507145
Koketso-dax <lanchaster.k@gmail.com>
2023-09-13 09:30:16 -0700

    Fixed checks to callbacks

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if (action != NULL && size > 0)
+	if (action != NULL && array != NULL)
 	{
 		for (x = 0; x < size; x++)
 		{
