dcf446e0245e5de9eb1bb459c70e46b999021075
Koketso-dax <lanchaster.k@gmail.com>
2023-09-13 09:15:25 -0700

    Added function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - will print a name
+ * @name: input name
+ * @f: callback function to print
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+/**
+ * array_iterator - will perform an operation on each [] element
+ * @array: input []
+ * @size: [] size
+ * @action: action ()
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t x;
+
+	for (x = 0; x < size; x++)
+	{
+		action(array[x]);
+	}
+}
