cedf3113a73e9f3783afd362a638088697bcb92a
Koketso-dax <lanchaster.k@gmail.com>
2023-09-15 17:03:03 -0700

    Fixed print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,15 +11,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int x;
 
-	if (separator == NULL)
-	{
-		return;
-	}
 	va_start(args, n);
 	for (x = 0; x < n; x++)
 	{
 		printf("%d", va_arg(args, int));
-		if (x < (n - 1))
+		if (x < (n - 1) && separator != NULL)
 		{
 			printf("%s", separator);
 		}
