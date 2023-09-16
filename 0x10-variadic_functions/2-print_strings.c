d9847d5adba310f47740f731c0ab052e441436df
Koketso-dax <lanchaster.k@gmail.com>
2023-09-15 17:25:33 -0700

    Added print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(args, char*);
 		if (s == NULL)
 		{
-			printf("%s", "nil");
+			printf("%s", "(nil)");
 		}
 		else
 		{
