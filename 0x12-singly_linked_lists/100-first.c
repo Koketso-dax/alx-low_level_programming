f69e6895aafa034ecd1df0417053ebfe01ff8bf2
Koketso-dax <lanchaster.k@gmail.com>
2023-09-21 14:42:23 -0700

    Added constructor

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * callback - will write to console before main
+ */
+
+void callback(void)
+{
+	printf("%s\n", "You're beat! and yet, you must allow,");
+	printf("%s\n", "I bore my house upon my back!");
+}
