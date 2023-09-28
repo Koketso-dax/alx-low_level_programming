3619ade4110c42c3597eeece49eb2a87c1d2cab2
Koketso-dax <lanchaster.k@gmail.com>
2023-09-28 06:29:00 -0700

    Fixed Return value for betty comment

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * get_endianess - will determine if int stores MSB or LSB at first bit
+ * get_endianness - will determine if int stores MSB or LSB at first bit
  * Return: First bit of var
  */
 int get_endianness(void)
