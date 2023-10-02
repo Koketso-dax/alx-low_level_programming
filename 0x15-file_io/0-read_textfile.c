68ec51589925498d65f3f2f4fc66491495005e28
Koketso-dax <lanchaster.k@gmail.com>
2023-10-02 15:54:15 -0700

    Fixed error return values

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,34 +11,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 	ssize_t  bytes_read, bytes_written;
 
-	if (!filename || letters == 0)
+	if (!filename || letters <= 0)
 	{
-		return (-1);
+		return (0);
 	}
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 	{
-		return (-1);
+		return (0);
 	}
 	buffer = malloc(letters);
 	if (!buffer)
 	{
 		close(file_descriptor);
-		return (-1);
+		return (0);
 	}
 	bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
 		free(buffer);
 		close(file_descriptor);
-		return (-1);
+		return (0);
 	}
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	free(buffer);
 	close(file_descriptor);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
-		return (-1);
+		return (0);
 	}
 	return (bytes_written);
 }
