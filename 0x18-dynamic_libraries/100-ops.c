518f9f12891ab86ac2b99c8600201bf74a9355fa
Koketso-dax <lanchaster.k@gmail.com>
2023-12-12 19:58:46 +0200

    Added 100 operations

---
+++ 0x18-dynamic_libraries/100-ops.c
@@ -0,0 +1,50 @@
+/**
+ * add - will return the sumation
+ * a: int input a
+ * b: int input b
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - will return the difference
+ * a: int input a
+ * b: int input b
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - will return the product
+ * a: int input a
+ * b: int input b
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - will return the quotient
+ * a: int input a
+ * b: int input b
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - will return the modulus
+ * a: int input a
+ * b: int input b
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
