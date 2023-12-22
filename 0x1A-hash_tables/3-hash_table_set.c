1bff3289cc65cc76ef9bae982ed8b31a5d3d0911
Koketso-dax <lanchaster.k@gmail.com>
2023-12-22 02:17:36 +0200

    Fixed Betty Errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -64,7 +64,7 @@ hash_node_t *create_node(const char *key, const char *value)
 
 	if (key == NULL || *key == '\0' || value == NULL)
 		return (0);
-	
+
 	new_node = malloc(sizeof(hash_node_t));
 
 	if (new_node == NULL)
@@ -82,14 +82,14 @@ hash_node_t *create_node(const char *key, const char *value)
 	}
 
 	new_node->value = strdup(value);
-	
+
 	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
-	
+
 	new_node->next = NULL;
 	return (new_node);
 }
