fbde98d8682e278ea5112811972d8f1fc9b38665
Koketso-dax <lanchaster.k@gmail.com>
2024-04-14 03:00:42 +0200

    Completed

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,50 +1,45 @@
 #include "search_algos.h"
 
 /**
- * print_array - Prints an array
- * @array: Pointer to the first element of the array
- * @left: Starting index of the array to print
- * @right: Ending index of the array to print
- */
-void print_array(int *array, size_t left, size_t right)
-{
-	size_t i = 0;
-
-	printf("Searching in array: ");
-	for (i = left; i <= right; i++)
-	{
-		printf("%d", array[i]);
-		if (i < right)
-			printf(", ");
-	}
-	printf("\n");
-}
-
-/**
- * binary_search - Searches for a value in a sorted array of integers
- *                 using the Binary search algorithm
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
- * @value: Value to search for
+ * binary_search - searches for a value in an integer array using a binary
+ * search algorithm, not guaranteed to return lowest index if `value` appears
+ * twice in `array`
+ * @array: pointer to first element of array to seach
+ * @size: number of elements in array
+ * @value: value to search for
  *
- * Return: Index where value is located, or -1 if not found
+ * Return: index containing `value`, or -1 if `value` not found or
+ * `array` is NULL
  */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size - 1, mid;
+	int low, mid, high;
+	int x;
 
 	if (array == NULL)
-		return (-1);
-	while (left <= right)
 	{
-		print_array(array, left, right);
-		mid = (left + right) / 2;
+		return (-1);
+	}
+
+	low = 0;
+	high = size - 1;
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (x = low; x <= high; x++)
+			printf("%i%s", array[x], x == high ? "\n" : ", ");
+
 		if (array[mid] < value)
-			left = mid + 1;
+			low = mid + 1;
 		else if (array[mid] > value)
-			right = mid - 1;
+			high = mid - 1;
 		else
 			return (mid);
 	}
+
 	return (-1);
 }
